2cce51fa591ac8027509ac3f9f3e0e62e0b7fd97
mickmihalich <mickmihalich@gmail.com>
2023-01-19 11:59:44 +0200

    add cart_item in cart_container

--- js/main.js
+++ js/main.js
@@ -2,3 +2,18 @@
 // то треба буде зафіксувати тут декілька змінних
 // для їх подальшої модернізації
 
+const addItems = document.querySelector('.add_new_item');
+
+function crateItem(){
+    const divItem = document.createElement('div');    
+    divItem.classList.add('cart_item');
+    console.log(divItem);
+    const cart_container_temp = document.querySelector('.cart_container');
+    cart_container_temp.append(divItem);
+}
+
+addItems.addEventListener('click', () => {
+    console.log('click by add new item');
+    crateItem();
+})
+
